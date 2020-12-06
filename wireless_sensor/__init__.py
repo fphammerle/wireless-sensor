9a5473483b2ad4cfb7348d550f9cf8af8b40d722
Fabian Peter Hammerle <fabian@hammerle.me>
2020-12-06 14:35:07 +0100

    added classes wrapping measurements

--- wireless_sensor/__init__.py
+++ wireless_sensor/__init__.py
@@ -1,3 +1,4 @@
+import abc
 import logging
 import math
 import struct
@@ -11,7 +12,73 @@ _MESSAGE_LENGTH_BITS = 65
 _MESSAGE_REPEATS = 3
 
 
-def _parse_transmission(signal: "numpy.ndarray(dtype=numpy.uint8)") -> None:
+class Measurement(abc.ABC):
+    # pylint: disable=too-few-public-methods
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        raise NotImplementedError()
+
+
+class TemperatureMeasurement(Measurement):
+    # pylint: disable=too-few-public-methods
+    def __init__(self, *, degrees_celsius: float):
+        self.degrees_celsius = degrees_celsius
+        super().__init__()
+
+    def __str__(self) -> str:
+        return "temperature {:.02f}°C".format(self.degrees_celsius)
+
+
+class RelativeHumidityMeasurement(Measurement):
+    # pylint: disable=too-few-public-methods
+    def __init__(self, value: float):
+        assert 0 <= value <= 1, value
+        self.value = value
+        super().__init__()
+
+    def __str__(self) -> str:
+        return "relative humidity {:.01f}%".format(self.value * 100)
+
+
+def _parse_message(
+    bits
+) -> typing.Tuple[TemperatureMeasurement, RelativeHumidityMeasurement]:
+    assert bits.shape == (_MESSAGE_LENGTH_BITS,), bits.shape
+    if (bits[:8] != 1).any():
+        raise ValueError("invalid prefix in message: {}".format(bits))
+    temperature_index, = struct.unpack(
+        ">H", numpy.packbits(bits[32:44])  # , bitorder="big")
+    )
+    # advertised range: [-40°C, +60°C]
+    # intercept: -40°C = -40°F
+    # slope estimated with statsmodels.regression.linear_model.OLS
+    # 12 bits have sufficient range: 2**12 * slope / 2**4 - 40 = 73.76
+    temperature = TemperatureMeasurement(
+        degrees_celsius=temperature_index / 576.077364 - 40
+    )
+    # advertised range: [10%, 99%]
+    # intercept: 0%
+    # slope estimated with statsmodels.regression.linear_model.OLS
+    # 12 bits have sufficient range: 2**12 * slope / 2**4 + 0 = 1.27
+    relative_humidity_index, = struct.unpack(
+        ">H", numpy.packbits(bits[44:56])  # , bitorder="big")
+    )
+    relative_humidity = RelativeHumidityMeasurement(
+        relative_humidity_index / 51451.432435
+    )
+    logging.debug(
+        "undecoded prefix %s, %s, %s, undecoded suffix %s",
+        numpy.packbits(bits[8:32]),  # address & battery?
+        temperature,
+        relative_humidity,
+        bits[56:],  # checksum?
+    )
+    return (temperature, relative_humidity)
+
+
+def _parse_transmission(
+    signal: "numpy.ndarray(dtype=numpy.uint8)"
+) -> typing.Tuple[TemperatureMeasurement, RelativeHumidityMeasurement]:
     bits = numpy.unpackbits(signal)[
         : _MESSAGE_LENGTH_BITS * _MESSAGE_REPEATS
     ]  # bitorder='big'
@@ -20,39 +87,8 @@ def _parse_transmission(signal: "numpy.ndarray(dtype=numpy.uint8)") -> None:
     if numpy.array_equal(repeats_bits[0], repeats_bits[1]) or numpy.array_equal(
         repeats_bits[0], repeats_bits[2]
     ):
-        _parse_message(repeats_bits[0])
-    else:
-        logging.debug("repeats do not match, ignoring transmission")
-
-
-def _parse_message(bits) -> None:
-    assert bits.shape == (_MESSAGE_LENGTH_BITS,), bits.shape
-    if (bits[:8] != 1).any():
-        logging.info("invalid prefix in message: %s", bits)
-        return
-    temperature_index, = struct.unpack(
-        ">H", numpy.packbits(bits[32:44])  # , bitorder="big")
-    )
-    # advertised range: [-40°C, +60°C]
-    # intercept: -40°C = -40°F
-    # slope estimated with statsmodels.regression.linear_model.OLS
-    # 12 bits have sufficient range: 2**12 * slope / 2**4 - 40 = 73.76
-    temperature_celsius = temperature_index / 576.077364 - 40
-    # advertised range: [10%, 99%]
-    # intercept: 0%
-    # slope estimated with statsmodels.regression.linear_model.OLS
-    # 12 bits have sufficient range: 2**12 * slope / 2**4 + 0 = 1.27
-    relative_humidity_index, = struct.unpack(
-        ">H", numpy.packbits(bits[44:56])  # , bitorder="big")
-    )
-    relative_humidity = relative_humidity_index / 51451.432435
-    logging.info(
-        "%s %.02f°C %.01f%% %s",
-        numpy.packbits(bits[8:32]),  # address & battery?
-        temperature_celsius,
-        relative_humidity * 100,
-        bits[56:],  # checksum?
-    )
+        return _parse_message(repeats_bits[0])
+    raise ValueError("repeats do not match")
 
 
 def _main():
@@ -98,9 +134,14 @@ def _main():
             packet = transceiver._get_received_packet()
             if packet:
                 logging.debug("%s", packet)
-                _parse_transmission(
-                    numpy.frombuffer(sync_word + packet.data, dtype=numpy.uint8)
-                )
+                try:
+                    measurements = _parse_transmission(
+                        numpy.frombuffer(sync_word + packet.data, dtype=numpy.uint8)
+                    )
+                except ValueError:
+                    logging.info("failed to decode %s", packet)
+                    continue
+                print(*measurements, sep="\t")
 
 
 if __name__ == "__main__":
